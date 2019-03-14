6ba8913778517569e0e729a47baa4f64932fe148
longhowlam <longhowlam@gmail.com>
2019-03-14 22:00:02 +0100

    text

--- textmining_spark.R
+++ textmining_spark.R
@@ -3,27 +3,37 @@ library(sparklyr)
 library(DBI)
 library(stringr)
 
+#### sentiment analysis ################################################################
+
 sc <- spark_connect(master = "local", version = "2.4.0")
 
-Jaap = readRDS( "JaapText.RDs") %>% filter(!is.na(prijs), longdescription != "" ) %>%  sample_n(size = 1000)
-Encoding(Jaap$longdescription) = "latin1"
+#### Import the data ####################################################################
+## non ascii characters need to be removed....
 
-Jaap = `JaapResults2017-05-28` %>% 
-  filter(!is.na(prijs), longdescription != "" ) %>% 
-  select(longdescription, prijs) %>% sample_n(size=100) %>% mutate(longdescription = str_trim(str_sub(longdescription,1,200)))
-Jaap$longdescription=  iconv(Jaap$longdescription, "latin1", "ASCII", sub="")
-jaap_tbl = copy_to(sc, Jaap)
+imdb <- read_csv("imdb_master.csv")  %>%  select(type, review, label)
+imdb$review = iconv(imdb$review, "latin1", "ASCII", sub="")
 
-test = data.frame(
-  text = c( "Hi I heard about Spark", "I wish Java could use case classes", "Logistic regression models are neat"),
-  target = c(0,0,1)
-)
+imdb_train = imdb %>% filter(type == "train") %>%  select(review, label)
+imdb_test = imdb %>% filter(type == "test")  %>%  select(review, label)
 
-test_tbl = copy_to(sc, test)
+imdb_tbl = copy_to(sc, imdb_train, overwrite = TRUE)
+imdb_test_tbl = copy_to(sc, imdb_test, overwrite = TRUE)
 
-testout = test_tbl %>%
-  ft_tokenizer(input_col = "text", output_col = "tokens") %>%
-  ft_count_vectorizer(input_col = "tokens", output_col = "counts") %>% 
-  ft_idf(input_col = "counts", out) 
+######  Train on logistic model, target is in column 'label'  #####################
 
-sdf_register(testout, "ppp")
+logregmodel = imdb_tbl %>%
+  ft_tokenizer(
+    input_col = "review",
+    output_col = "tokens"
+  ) %>%
+  ft_count_vectorizer(
+    input_col = "tokens", 
+    output_col = "counts"
+  ) %>% 
+  ft_idf(
+    input_col = "counts", 
+    output_col = "features"
+  ) %>% 
+  ml_logistic_regression(reg_param = 0.5)
+
+summary(testout)
